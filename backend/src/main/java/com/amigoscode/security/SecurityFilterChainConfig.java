fc6c5dcaabc247f69501f1372d10ad00ab47bf5f
cimusini <musimecisimone@gmail.com>
2023-09-15 18:49:51 +0200

    Expose health and info metrics

--- backend/src/main/java/com/amigoscode/security/SecurityFilterChainConfig.java
+++ backend/src/main/java/com/amigoscode/security/SecurityFilterChainConfig.java
@@ -44,7 +44,8 @@ public class SecurityFilterChainConfig {
                 .permitAll()
                 .requestMatchers(
                         HttpMethod.GET,
-                        "/ping"
+                        "/ping",
+                        "/actuator/**"
                 )
                 .permitAll()
                 .anyRequest()
